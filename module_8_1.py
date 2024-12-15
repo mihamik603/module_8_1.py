404b21eb3393f2635ec4d4e49c1fc58c6340d3d4
mihamik603 <vmm1331@gmail.com>
2024-12-15 20:49:22 +0400

    Add files via upload

---
+++ module_8_1.py
@@ -0,0 +1,15 @@
+def add_everything_up(a, b):
+    try:
+        if isinstance(a, str) and isinstance(b, str):
+            return a + b
+        elif isinstance(a, (int, float)) and isinstance(b, (int, float)):
+            return a + b
+        else:
+            return str(a) + str(b)
+    except TypeError:
+        return str(a) + str(b)
+
+# Примеры использования
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
